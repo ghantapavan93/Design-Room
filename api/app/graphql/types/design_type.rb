9de26c51c635b7a076c7473faf84fd0220e960bf
ghantapavan93 <ghantapavan93@gmail.com>
2026-02-25 19:09:44 -0600

    Fix schema mismatch: add missing fields to DesignType

--- api/app/graphql/types/design_type.rb
+++ api/app/graphql/types/design_type.rb
@@ -1,7 +1,11 @@
 module Types
   class DesignType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: false
+    field :title, String, null: true
+    field :homeowner_name, String, null: true
+    field :address, String, null: true
+    field :status, String, null: true
+    field :cover_image_url, String, null: true
     field :final_version_id, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     
